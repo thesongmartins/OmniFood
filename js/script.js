dab7282029d03d0ba21e8bbc41f7dc4383883972
thesongmartins <songmartins01@gmail.com>
2024-12-31 14:48:04 +0100

    Optimizations: Sticky Nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,84 @@
+console.log('Hello World!');
+
+// const myName = 'John Doe';
+// console.log(myName);
+// const h1 = document.querySelector('.heading-primary');
+// console.log(h1);
+
+// h1.textContent = myName;
+// h1.style.backgroundColor = 'red';
+// h1.style.padding = '5rem';
+
+// h1.addEventListener('click', function () {
+//     h1.textContent = myName;
+//     h1.style.backgroundColor = 'red';
+//     h1.style.padding = '5rem';
+// });
+const sectionHeroEl = document.querySelector('.section-hero')
+// Set Current year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear(); // Getting current year
+console.log(currentYear);
+yearEl.textContent = currentYear;
+// yearEl.textContent = new Date().getFullYear();
+
+// Mobile Navigation
+const btnNavEl = document.querySelector('.btn-mobile-nav'); // Selecting mobile nav button
+const headerEl = document.querySelector('.header'); // Selecting header element
+
+btnNavEl.addEventListener('click', function () {
+    headerEl.classList.toggle('nav-open');
+});
+
+// Smooth Scrolling animation
+const allLinks = document.querySelectorAll('a:link'); // Select all links
+console.log(allLinks);
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Preventing default action
+        const href = link.getAttribute('href');
+        // console.log(href);
+
+        // Scroll back to top
+        if (href === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+
+
+        }
+
+        // Scroll to other links
+        if (href !== '#' && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: 'smooth' });
+        }
+        // Close Mobile Nav
+        if (link.classList.contains('nav-link')) {
+            headerEl.classList.remove('nav-open'); // Close mobile nav
+        }
+    });
+});
+
+// Sticky Navigation
+const obs = new IntersectionObserver(function (entries) {
+    const ent = entries[0]; // Destructuring
+    console.log(ent);
+    if (!ent.isIntersecting) {
+        document.querySelector('.header').classList.add('sticky'); // Add sticky class
+    } else {
+        document.querySelector('.header').classList.remove('sticky'); // Remove sticky class
+    }
+}, {
+    root: null,
+    threshold: 0,
+});
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement("div");
+    let flex = document.createElement("div");
     flex.style.display = "flex";
     flex.style.flexDirection = "column";
     flex.style.rowGap = "1px";
@@ -10,7 +87,7 @@ function checkFlexGap() {
     flex.appendChild(document.createElement("div"));
 
     document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
+    let isSupported = flex.scrollHeight === 1;
     flex.parentNode.removeChild(flex);
     console.log(isSupported);
 
